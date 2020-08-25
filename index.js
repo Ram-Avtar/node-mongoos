476f7931c9d3bd144c24d22acc9e1a5608a65b93
ram29avtar@gmail.com <ram29avtar@gmail.com>
2020-08-25 14:45:16 +0530

    mongoose part three

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useFindAndModify:false,
+    useCreateIndex:true
+});
+
+
 
 connect.then((db) => {
 
@@ -16,12 +23,24 @@ connect.then((db) => {
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({});
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set:{description:'updated test'}
+        },{
+            new:true
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
-
-        return Dishes.remove({});
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating:5,
+            comment:'I\'m getting a sinking feeling!',
+            author:'Leonardo di Carpaccio'
+        })
+        return dish.save();
+    })
+    .then((dish)=>{
+        console.log(dish)
+        return Dishes.deleteMany({});
     })
     .then(() => {
         return mongoose.connection.close();
